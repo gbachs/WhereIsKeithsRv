39e806fb9addc4788d7514282ed91bac937f3cef
nkeen <33171251+nkeen@users.noreply.github.com>
2017-10-28 06:08:13 -0400

    Don't combine libraries

--- Source/whereiskeithsrv/Content/scripts/app.js
+++ Source/whereiskeithsrv/Content/scripts/app.js
@@ -6,10 +6,10 @@ requirejs.config({
     enforceDefine: false,
 
     paths: {
-        jquery: "lib/jquery-2.1.1.min",         // [ "//code.jquery.com/jquery-2.1.1.min", "libs/jquery-2.1.1.min" ]
-        knockout: "lib/knockout-3.1.0.min",     // [ "//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min", "libs/knockout-3.1.0.min" ]
-        moment: "lib/moment-2.7.0.min",         // [ "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min", "libs/moment-2.7.0.min" ]
-        underscore: "lib/underscore-1.6.0.min"  // [ "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min", "libs/underscore-1.6.0.min" ]
+        jquery: "lib/jquery-2.1.1.min",         // [ "//code.jquery.com/jquery-2.1.1.min", "lib/jquery-2.1.1.min" ]
+        knockout: "lib/knockout-3.1.0.min",     // [ "//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min", "lib/knockout-3.1.0.min" ]
+        moment: "lib/moment-2.7.0.min",         // [ "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min", "lib/moment-2.7.0.min" ]
+        underscore: "lib/underscore-1.6.0.min"  // [ "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min", "lib/underscore-1.6.0.min" ]
     }
 });
 
